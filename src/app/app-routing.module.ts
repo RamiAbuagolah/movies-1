31c917e5c288a21bc538b2c1224076e4a90fac8c
Moayyad Alajlouni <moaead-alajlouni@hotmail.com>
2021-10-31 15:01:07 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BestRatingComponent } from './best-rating/best-rating.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:"best",component:BestRatingComponent},
+  {path:"",component:HomeComponent} 
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
